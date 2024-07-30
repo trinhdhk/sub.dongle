fc3893cf4db2be909ced3df900a59764262723f4
Trinh Dong <46802143+trinhdhk@users.noreply.github.com>
2024-07-30 10:26:14 +0700

    improve param

--- R/forestploter.R
+++ R/forestploter.R
@@ -4,41 +4,48 @@
 #' @param x a tibble create by tidy_subgroup
 #' @param terms <character> which term to show forest plot
 #' @param label [\code{"Subgroup"}] A character specifying the label of the subgroup column
-#' @param .right_stats [\code{c('Est (95% CI)' = '{estimate} ({conf.low}, {conf.high})', 'p-value' =  '{format.pval(p.value, digits=2, eps=1e-3)}')}]
+#' @param stats [\code{c('Est. (95% CI)' = format.ci(estimate, conf.low, conf.high, digits=1), 'p-value' =  format.pval(p.value, digits=2, eps=1e-3))}]
 #' stat names in x to be shown in the forest plot after the CI bar
-#' @param .left_stats [\code{NULL}] <NULL|character> stat name in x to forest plot that are shown before the CI
+#' @param plot_pos [\code{2}] <int> column position of plot
+#' @param plot_width [\code{1}] <numeric> adjust plot width by a factor > 0
+#' @param arrow_lab [\code{c('Harmful', 'Benefit')}] label of the arrow
 #' @param ... additional parameters passed to [forestploter::forest()].
 #' @importFrom rlang enquo
 #' @return a plot
 #' @export
-foresploter <- function(x, ...){
-  UseMethod('foresploter')
+forestploter <- function(x, ...){
+  UseMethod('forestploter')
 }
 
-#' @rdname foresploter
+#' @rdname forestploter
 #' @export
-foresploter.default <- forestploter::forest
+forestploter.default <- forestploter::forest
 
-#' @rdname foresploter
-#' @method foresploter tidy_subgroup_tbl
+#' @rdname forestploter
+#' @method forestploter tidy_subgroup_tbl
 #' @export
-foresploter.tidy_subgroup_tbl <-
+forestploter.tidy_subgroup_tbl <-
   function(x,
            terms = x$term[[2]],
            label ="Subgroup",
-           .right_stats = c('Est. (95% CI)' = '{format.ci(estimate, conf.low, conf.high, digits=1)}',
-                            'p-value' =  '{format.pval(p.value, digits=2, eps=1e-3)}'),
-           .left_stats = NULL,
-           .show_p.value = TRUE,
+           stats = c('Est. (95% CI)' = format.ci(estimate, conf.low, conf.high, digits=1),
+                     'p-value' =  format.pval(p.value, digits=2, eps=1e-3)),
+           plot_pos = 2L,
+           plot_width = 1,
+           arrow_lab = c('Harmful', 'Benefit'),
            ...){
+    if (plot_width <= 0)
+      stop(cli::cli_abort("plot_width must be strictly positive."))
+    stats <- rlang::enexpr(stats)
+    if (as.character(stats[[1]]) == 'c') stats <- stats[-1]
+    stats <- stats[names(stats) != '']
     ell <- list(...)
-    prepared_dt <- ._prepare_forest_dt_(x, terms, label, .right_stats, .left_stats)
+    prepared_dt <- ._prepare_forest_dt_(x, terms, label, stats, plot_pos, plot_width)
     plt <- with(prepared_dt,
                 forestploter::forest(
                   data = prepared_dt |>
                     dplyr::select(-estimate, -conf.low, -conf.high, -std.error,
-                           dplyr::any_of(names(.left_stats)),
-                           dplyr::any_of(names(.right_stats))) |>
+                           dplyr::any_of(names(stats))) |>
                     dplyr::mutate(
                       dplyr::across(dplyr::everything(),
                                     ~ ifelse(is.na(.x)|.x=='NA','',.x))
@@ -47,18 +54,20 @@ foresploter.tidy_subgroup_tbl <-
                   lower = conf.low,
                   upper = conf.high,
                   sizes = std.error,
-                  ci_column = if (is.null(ell$ci_column)) 2 else ell$ci_column
+                  ci_column = plot_pos,
+                  arrow_lab = arrow_lab,
+                  ...
                 ))
     plt
   }
 
 
-._prepare_forest_dt_ <- function(x, terms, label, .right_stats, .left_stats){
+._prepare_forest_dt_ <- function(x, terms, label, stats, plot_pos, plot_width){
   dt <- dplyr::filter(x, term %in% terms)
   # dt$.subgroup <- dt$.subgroup_name
-  all_stats <-  c(.right_stats, .left_stats)
-  for (i in seq_along(all_stats)){
-    dt[names(all_stats)[[i]]] <- with(dt,glue::glue(all_stats[[i]]))
+  # browser()
+  for (i in seq_along(stats)){
+    dt[names(stats)[[i]]] <- with(dt,eval(stats[[i]]))
   }
   dt <- dplyr::group_by(dt, .subgroup_name) |>
     dplyr::group_modify(
@@ -75,14 +84,14 @@ foresploter.tidy_subgroup_tbl <-
       }
     ) |>
     ungroup() |>
-    mutate(" " = paste(rep(" ", 30), collapse = " "))
+    mutate(" " = paste(rep(" ", 30 * plot_width), collapse = " "))
 
   select(dt, "{label}" := .subgroup_val,
-         names(.left_stats),
+         names(stats)[0:(plot_pos-2)],
          ` `,
          estimate,
          std.error, conf.low, conf.high,
-         names(.right_stats))
+         names(stats)[(plot_pos-1):length(stats)])
 }
 
 format.ci <- function(est, l95, u95, digits=1){
